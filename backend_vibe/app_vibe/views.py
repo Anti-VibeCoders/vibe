e9056a393809a9fc1de92204010e99f1d37d6d05
Mathias Rendon <mathiassaid7@gmail.com>
2025-05-22 20:09:31 -0500

    feat(views): add view sets for Publicacion, Comentario, and Seguidor
    This commit introduces new view sets for handling Publicacion, Comentario, and Seguidor models, enabling CRUD operations for these entities in the API. The existing UserViewSet remains unchanged.

--- backend_vibe/app_vibe/views.py
+++ backend_vibe/app_vibe/views.py
@@ -1,8 +1,20 @@
 from rest_framework import viewsets
-from .serializer import UserSerializer
-from .models import User
+from .serializer import UserSerializer, PublicacionSerializer, ComentarioSerializer, SeguidorSerializer
+from .models import User, Publicacion, Comentario, Seguidor
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
-    queryset =User.objects.all()
-    serializer_class=UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PublicacionViewSet(viewsets.ModelViewSet):
+    queryset = Publicacion.objects.all()
+    serializer_class = PublicacionSerializer
+
+class ComentarioViewSet(viewsets.ModelViewSet):
+    queryset = Comentario.objects.all()
+    serializer_class = ComentarioSerializer
+
+class SeguidorViewSet(viewsets.ModelViewSet):
+    queryset = Seguidor.objects.all()
+    serializer_class = SeguidorSerializer
