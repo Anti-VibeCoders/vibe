1dd592de3de12f8bf93eba6fb2cc989f069ba063
Mathias Rendon <mathiassaid7@gmail.com>
2025-05-22 21:50:17 -0500

    feat(auth): add login and register pages with form validation
    Introduce new pages for user authentication, including `/login` and `/register` routes. Both pages utilize `react-hook-form` and `zod` for form validation and submission. The Login page includes fields for email and password, while the Register page adds username and password confirmation fields. This change enables users to authenticate and create accounts within the application.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,22 @@
 import App from './App'
+import Login from '@/pages/Login'
 import Error from '@/pages/Error'
+import Register from '@/pages/Register'
 
 const routes = [
     {
         path: '/',
         element: <App />,
+        errorElement: <Error />
+    },
+    {
+        path: '/login',
+        element: <Login />,
+        errorElement: <Error />
+    },
+    {
+        path: '/register',
+        element: <Register />,
         errorElement: <Error />
     }
 ]
