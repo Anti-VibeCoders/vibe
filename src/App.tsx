54c43a4966f00753d5d87ed04d71f2b434b46062
Mori <123713150+MoriNo23@users.noreply.github.com>
2025-06-01 12:28:25 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,4 @@
 
-import { Outlet, useLocation } from "react-router-dom";
-import Home from "@/pages/Home";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-function App() {
-  const location = useLocation();
-  const home = location.pathname === "/";
-  /* me gusta el pito aqui */
-  return (
-    <>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        {home ? (
-          <div className="homepage_container">
-            <Home />
-          </div>
-        ) : (
-          <div className="outlet-container w-full flex-1 flex justify-center items-center">
-            <Outlet />
-          </div>
-        )}
-        <Footer />
-      </div>
-
 import Login from './pages/Login'
 
 function App() {
