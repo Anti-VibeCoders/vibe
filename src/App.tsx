5171ff06f740499070b46da44d4d1b72ade20d8b
MoriNo <atnysin@hotmail.com>
2025-06-01 11:54:01 -0400

    refactor: mejorar estructura de la UI y responsive design
    - Reorganizar clases CSS con patrón BEM para mejor mantenibilidad
    - Mejorar diseño responsive en componentes Header, Post y SideNav
    - Mover estilos inline a clases CSS en index.css
    - Actualizar dependencias en package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import { Outlet, useLocation } from 'react-router-dom'
-import Home from '@/pages/Home'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Outlet, useLocation } from "react-router-dom";
+import Home from "@/pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  const location = useLocation()
-  const home = location.pathname === '/'
-
+  const location = useLocation();
+  const home = location.pathname === "/";
+  /* me gusta el pito aqui */
   return (
     <>
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      {home ? (
-        <div className="home-container w-full flex-1 flex justify-center items-center">
-          <Home />
-        </div>
-      ) : (
-        <div className="outlet-container w-full flex-1 flex justify-center items-center">
-          <Outlet />
-        </div>
-      )}
-      <Footer />
-    </div>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        {home ? (
+          <div className="homepage_container">
+            <Home />
+          </div>
+        ) : (
+          <div className="outlet-container w-full flex-1 flex justify-center items-center">
+            <Outlet />
+          </div>
+        )}
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
