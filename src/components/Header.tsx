4d0e4725dae4e2b4c880ab3bb1315ff9e2d9c5ee
Mathias Rendon <mathiassaid7@gmail.com>
2025-06-03 11:38:41 -0500

    style: update notifications card width to full for better responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -143,49 +143,6 @@ function Header() {
       </header>
     </>
   );
-
-
-          <Link to="messages">
-            <MessageSquare className="cursor-pointer size-5" />
-          </Link>
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Avatar className="cursor-pointer">
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback className="bg-blue-600 font-semibold text-black text-xl">
-                  A
-                </AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="dark:bg-neutral-950 dark:text-white mr-4 border dark:border-gray-600">
-              <DropdownMenuLabel>Mi Cuenta</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <Link to="profile">
-                <DropdownMenuItem className="cursor-pointer">
-                  Perfil
-                </DropdownMenuItem>
-              </Link>
-              <Link to="/home">
-                <DropdownMenuItem className="cursor-pointer">
-                  Pagos
-                </DropdownMenuItem>
-              </Link>
-              <Link to="/home">
-                <DropdownMenuItem className="cursor-pointer">
-                  Equipo
-                </DropdownMenuItem>
-              </Link>
-              <Link to="/">
-                <DropdownMenuItem className="cursor-pointer">
-                  Cerrar sesión
-                </DropdownMenuItem>
-              </Link>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      </header>
-    </>
-  );
 }
 
 export default Header;
