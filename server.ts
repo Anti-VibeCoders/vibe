82eda94b46af64b6eb77a4461ceb63877561ce74
Mathias Rendon <mathiassaid7@gmail.com>
2025-06-03 15:42:19 -0500

    fix: correct formatting in error handling response for WebSocket messages

--- server.ts
+++ server.ts
@@ -1,18 +1,17 @@
 import { WebSocketServer, WebSocket } from 'ws';
 
 const ws = new WebSocketServer({ port: 7173 })
-
 const users = new Set<WebSocket>()
 
 ws.on('connection', (socket) => {
     users.add(socket)
-    
+
     socket.on('message', (msg: any) => {
         let data
         try {
             data = JSON.parse(msg)
         } catch (error) {
-            data = { nombre: "Desconocido", mensaje: "Hubo un error", pfp: 'https://github.com/shadcn.png'}
+            data = { nombre: "Desconocido", mensaje: "Hubo un error", pfp: 'https://github.com/shadcn.png' }
             console.log(error)
         }
         users.forEach(user => {
