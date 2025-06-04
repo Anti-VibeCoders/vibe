adca28eb82dc94c862c79261213732277442af91
CodeFoxZ37 <130168476+CodeFoxZ37@users.noreply.github.com>
2025-06-04 08:43:51 -0400

    Cambio en las funciones de los views(auth_views.py, profile_views.py), las funciones de estos archivos pasaron a ser clases para un mejor manejo y se cambiaron las urls de estas clases

--- backend_vibe/app_vibe/urls.py
+++ backend_vibe/app_vibe/urls.py
@@ -3,16 +3,16 @@ from app_vibe.view import auth_views, user_views, profile_views, conexion_views
 
 urlpatterns = [
     # auths
-    path('auth/register/', auth_views.RegistroView, name='auth-register'),
-    path('auth/login/', auth_views.LoginView, name='auth-login'),
-    path('auth/logout/', auth_views.LogoutView, name='auth-logout'),
+    path('auth/register/', auth_views.RegistroViewSet.as_view(), name='auth-register'),
+    path('auth/login/', auth_views.LoginViewSet.as_view(), name='auth-login'),
+    path('auth/logout/', auth_views.LogoutViewSet.as_view(), name='auth-logout'),
     
     # user
     path('users/<int:user_id>/', user_views.user_detail, name='user-detail'),
     path("users/<int:user_id>/config/", user_views.user_config, name="user-config"),
     
     # profile
-    path('profile/', profile_views.profileView, name='logout'),
+    path('profile/', profile_views.UserProfileViewSet.as_view(), name='logout'),
     
     # publicactions
     path('Publications/', conexion_views.PublicacionView, name='Publicacion'),
