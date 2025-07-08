512c377d61d7407cea008accecccd732be37517b
Lobosanplay <parra.santiago2008@gmail.com>
2025-07-08 14:06:02 -0500

    Eliminating folders/files that are not used, connecting with Supabase, Requirements.txt update

--- backend_vibe/backend_vibe/settings.py
+++ backend_vibe/backend_vibe/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -94,9 +98,6 @@ DATABASES = {
 
 
 AUTH_USER_MODEL = 'app_vibe.User'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
