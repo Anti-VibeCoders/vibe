071bff2f5f51647b788f85e76e2c28fa711e945b
Lobosanplay <parra.santiago2008@gmail.com>
2025-07-08 12:00:34 -0500

    Changes in the User variable within the Auth_views file and changes in Settings for Connection with Suppabase

--- backend_vibe/backend_vibe/settings.py
+++ backend_vibe/backend_vibe/settings.py
@@ -79,11 +79,19 @@ WSGI_APPLICATION = "backend_vibe.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': os.getenv("dbname"),
+                'USER': os.getenv("user"),  
+                'PASSWORD': os.getenv("password"),  
+                'HOST': os.getenv("host"),  
+                'PORT': os.getenv("port"),
+                'OPTIONS': {
+                    'sslmode': 'require', 
+                },
+            }
+        }
+
 
 AUTH_USER_MODEL = 'app_vibe.User'
 
