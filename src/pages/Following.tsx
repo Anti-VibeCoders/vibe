3199b772dd7a90f9ec1e7c3610d52256545ea85f
CodeFoxZ37 <arammusset7@gmail.com>
2025-07-07 21:08:42 -0400

    Mejora en el diseño del contenedor del button ahora ocupa mas espacio y al boton de siguiendo se le añadio un color de fondo y mas estilo

--- src/pages/Following.tsx
+++ src/pages/Following.tsx
@@ -3,16 +3,17 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 function Following(){
     return (
     <>
-    <div className="flex justify-center items-center w-full">
-        <div className="flex justify-between w-1/2 h-auto bg-zinc-900 rounded-lg p-5 border-neutral-500 border-1">
+    <div className="flex flex-col justify-center items-center w-full">
+        
+        <div className="flex justify-between items-center w-3/4 h-auto bg-zinc-900 rounded-lg p-4 border-neutral-500 border-1">
             <div className="flex flex-1 gap-5 items-center w-full">
                 <Avatar className="w-15 h-15">
                  <AvatarImage src={"https://randomuser.me/api/portraits/men/32.jpg"} />
                   </Avatar>
-                <b className="text-lg">Juanito Perez</b>
+                <b className="text-lg font-semibold">Juanito Perez</b>
             </div>
         <button
-        className=" border-neutral-500 border-1 px-2 h-auto rounded-md"
+        className="bg-blue-500 px-4 py-2  rounded-md text-black text-sm font-semibold"
         >Siguiendo
         </button>
         </div>
