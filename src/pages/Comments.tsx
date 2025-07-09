dee9df3b6fc266359d4ff92958ac9daab4852079
RamCodeZ3 <arammusset7@gmail.com>
2025-07-09 16:24:45 -0400

    se agrego la funcion calculeTime para calcular el tiempo que ha pasado en dia desde que se escribio el comentario y se mejoraron algunos destalles

--- src/pages/Comments.tsx
+++ src/pages/Comments.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { useState, useEffect } from "react";
-import { Heart, MoreHorizontal } from "lucide-react";
+import { Heart, MoreHorizontal, Clock4Icon } from "lucide-react";
+import { Link, useNavigate } from 'react-router-dom'
 
 function Comments(){
     type Posts = {
@@ -19,6 +20,8 @@ function Comments(){
         avatarUser: string;
         body: string;
         like: number;
+        date: string;
+        activated: boolean;
     }
 
     const [post, setPost] = useState<Posts[]>([])
@@ -36,6 +39,7 @@ function Comments(){
                 avatarUser: "https://randomuser.me/api/portraits/men/32.jpg",
                 body: "Donde se encuentra ese museo",
                 like: 23,
+                date: "2024-11-20T12:15:00Z",
             },
             {
                 id: 1,
@@ -43,6 +47,7 @@ function Comments(){
                 avatarUser: "https://randomuser.me/api/portraits/women/44.jpg",
                 body: "Kirby es super Cuuuuuuuute!! 😍😍😍",
                 like: 56,
+                date: "2025-05-20T12:15:00Z",
             },
             {
                 id: 2,
@@ -50,6 +55,7 @@ function Comments(){
                 avatarUser: "https://randomuser.me/api/portraits/men/53.jpg",
                 body: "De hecho el nombre kirby fue elegido por Shigeru Miyamoto por su sonoridad y porque se asemejaba a la personalidad del personaje. Curiosamente, el nombre también está vinculado a una disputa legal entre Nintendo y el abogado John Kirby.",
                 like: 34,
+                date: "2025-07-08T12:15:00Z",
             },
             {
                 id: 3,
@@ -57,6 +63,7 @@ function Comments(){
                 avatarUser: "https://randomuser.me/api/portraits/lego/2.jpg",
                 body: "Nah esa cosa es re horrible, ademas me quiero dar un tiro >:C",
                 like: 13,
+                date: "2024-09-20T12:15:00Z",
             },
             {
                 id: 4,
@@ -64,12 +71,14 @@ function Comments(){
                 avatarUser: "https://randomuser.me/api/portraits/men/53.jpg",
                 body: "El verdadero kirby modo zelda",
                 like: 46,
+                date: "2025-03-18T12:15:00Z",
             },
         ]
        }
        setComment(
                 response.data.map((cmt: any) => ({
                     ...cmt,
+                    activated: false
                 })))
 
     }catch (err) {
@@ -87,6 +96,23 @@ function Comments(){
         return () => clearInterval(interval);
     }, []);
 
+    const calculeTime = (date: string) => {
+        const newDate = new Date(date);
+        const now = new Date();
+        const difference = now.getTime() - newDate.getTime();
+
+        const second = Math.floor(difference / 1000);
+        const mminute = Math.floor(second / 60);
+        const hour = Math.floor(mminute / 60);
+        const days = Math.floor(hour / 24);
+
+        if (second < 60) return "hace un momento";
+        if (mminute < 60)
+            return `hace ${mminute} minuto${mminute !== 1 ? "s" : ""}`;
+        if (hour < 24) return `hace ${hour} hora${hour !== 1 ? "s" : ""}`;
+        return `hace ${days} día${days !== 1 ? "s" : ""}`;
+    };
+
     if (loading) return <div>Cargando pagina...</div>;
     if (error) return <div className="text-red-500">{error}</div>;
     
@@ -102,9 +128,9 @@ function Comments(){
 
                 </div>
             </div>
-            <div className="container-comment flex flex-col gap-1 flex-1 w-full h-auto border-l-1 items-center overflow-y-scroll overflow-hidden">
+            <div className="container-comment flex flex-col gap-1.5 flex-1 w-full h-auto border-l-1 items-center overflow-y-scroll overflow-hidden">
                 {comment.map((cmt) =>(
-                    <div className="flex gap-3 border-1 rounded-lg px-2 py-2 w-3/4">
+                    <div className="flex gap-3 border-1 rounded-lg px-2 py-2 w-[90%]">
                         <div className="flex gap-3">
                             <Avatar className="w-10 h-10">
                                 <AvatarImage src={cmt.avatarUser} /> 
@@ -112,16 +138,32 @@ function Comments(){
                         </div>
                         <div className="w-full">
                             <div className="w-full flex justify-between">
-                                <span className="font-semibold">{cmt.username} </span>
-                                <MoreHorizontal className="size-4 dark:text-white inline-block align-middle"></MoreHorizontal>
+                                <Link to="/home/profile" className="font-semibold cursor-pointer hover:text-blue-500 hover:underline">{cmt.username} </Link>
+                                <MoreHorizontal className="size-4 dark:text-white inline-block align-middle cursor-pointer"></MoreHorizontal>
                             </div>
                               <div className="flex flex-col gap-4">
                             <p className="text-sm">{cmt.body}</p>
 
-                           <div className="flex gap-2 items-center text-sm">
-                             <Heart className="size-4 dark:text-white inline-block align-middle" /> 
+                           <div  className="flex gap-15 !text-gray-500">
+                            <div className="flex gap-2 items-center text-sm ">
+                             <Heart
+                               className={`size-4 inline-block align-middle cursor-pointer transition-all duration-200 
+                             hover:text-red-500 ${cmt.activated ? "fill-red-500 text-red-500" : "text-gray-500"}`}
+                                onClick={() => {
+                                  setComment(comment.map((item) =>
+                                   item.id === cmt.id ? { ...item, activated: !item.activated } : item
+                                    ));
+                                }}
+                                fill={cmt.activated ? "red" : "none"}
+                               />
+
                              {cmt.like} 
                              <span className="">Me gustas</span>
+                           </div>
+                           <div className="flex gap-2 items-center text-sm">
+                             <Clock4Icon className="size-4 inline-block align-middle" />  
+                             {calculeTime(cmt.date)}
+                           </div>
                            </div>
                         </div>
                         </div>
