a85f0de818072662d8d8396ef75343528dc7da41
RamCodeZ3 <arammusset7@gmail.com>
2025-07-09 14:43:31 -0400

    se hizo la primera parte de la seccion de commentario ahora se ven los comentarios ya cargados.

--- src/pages/Comments.tsx
+++ src/pages/Comments.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { useState, useEffect } from "react";
+import { Heart, MoreHorizontal } from "lucide-react";
 
 function Comments(){
     type Posts = {
@@ -47,7 +48,7 @@ function Comments(){
                 id: 2,
                 username: "Said Ruíz",
                 avatarUser: "https://randomuser.me/api/portraits/men/53.jpg",
-                body: "Sabias que El nombre kirby fue elegido por Shigeru Miyamoto por su sonoridad y porque se asemejaba a la personalidad del personaje. Curiosamente, el nombre también está vinculado a una disputa legal entre Nintendo y el abogado John Kirby.",
+                body: "De hecho el nombre kirby fue elegido por Shigeru Miyamoto por su sonoridad y porque se asemejaba a la personalidad del personaje. Curiosamente, el nombre también está vinculado a una disputa legal entre Nintendo y el abogado John Kirby.",
                 like: 34,
             },
             {
@@ -56,6 +57,13 @@ function Comments(){
                 avatarUser: "https://randomuser.me/api/portraits/lego/2.jpg",
                 body: "Nah esa cosa es re horrible, ademas me quiero dar un tiro >:C",
                 like: 13,
+            },
+            {
+                id: 4,
+                username: "Said Ruíz",
+                avatarUser: "https://randomuser.me/api/portraits/men/53.jpg",
+                body: "El verdadero kirby modo zelda",
+                like: 46,
             },
         ]
        }
@@ -94,17 +102,28 @@ function Comments(){
 
                 </div>
             </div>
-            <div className="container-comment flex flex-col flex-1 h-full border-l-1">
+            <div className="container-comment flex flex-col gap-1 flex-1 w-full h-auto border-l-1 items-center overflow-y-scroll overflow-hidden">
                 {comment.map((cmt) =>(
-                    <div className="w-1/2">
-                        <div className="flex items-center gap-3">
+                    <div className="flex gap-3 border-1 rounded-lg px-2 py-2 w-3/4">
+                        <div className="flex gap-3">
                             <Avatar className="w-10 h-10">
                                 <AvatarImage src={cmt.avatarUser} /> 
                             </Avatar>
-                            <span className="font-semibold">{cmt.username}</span>
                         </div>
-                        <div className="">
+                        <div className="w-full">
+                            <div className="w-full flex justify-between">
+                                <span className="font-semibold">{cmt.username} </span>
+                                <MoreHorizontal className="size-4 dark:text-white inline-block align-middle"></MoreHorizontal>
+                            </div>
+                              <div className="flex flex-col gap-4">
                             <p className="text-sm">{cmt.body}</p>
+
+                           <div className="flex gap-2 items-center text-sm">
+                             <Heart className="size-4 dark:text-white inline-block align-middle" /> 
+                             {cmt.like} 
+                             <span className="">Me gustas</span>
+                           </div>
+                        </div>
                         </div>
                     </div>
                 ))}
