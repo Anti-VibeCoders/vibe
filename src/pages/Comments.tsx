b2cb3aed6cc070e12db0ce6bb869f4424edd9149
RamCodeZ3 <arammusset7@gmail.com>
2025-07-11 17:35:49 -0400

    Refactorizacion: Se creo un nuevo componente DropMenuPost para no tener que copiado los dos dropMenus en diferentes archivos, ahora si se hara un cambio afectada a todos menos a uno.

--- src/pages/Comments.tsx
+++ src/pages/Comments.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/dialog"
 import { Textarea } from '../components/ui/textarea'
 import { Badge } from "@/components/ui/badge"
-import { toast } from 'sonner'
+import DropMenuPost from '@/components/ui/DropMenuPost'
 
 function Comments() {
     type Posts = {
@@ -185,75 +185,7 @@ function Comments() {
                             </div>
                             <span className="text-sm text-gray-500">{p.username} • {calculeTime(p.date)}</span>
                             </div>
-                            <DropdownMenu>
-                            <DropdownMenuTrigger className='cursor-pointer'>
-                                <Button variant="ghost" size="icon" className="text-zinc-400 cursor-pointer hover:text-white">
-                                    <MoreHorizontal className="h-4 w-4" />
-                                </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent>
-                                <Dialog>
-                                    <DialogTrigger asChild>
-                                        <DropdownMenuItem className="cursor-pointer text-red-400 hover:!text-red-400" onSelect={e => e.preventDefault()}>
-                                            <MessageSquareWarningIcon className="text-red-400" /> <span className="w-full">Reportar</span>
-                                        </DropdownMenuItem>
-                                    </DialogTrigger>
-                                    <DialogContent>
-                                        <DialogHeader>
-                                            <DialogTitle>¿Por qué quieres reportar este post?</DialogTitle>
-                                            <DialogDescription>
-                                                Por favor, describe el motivo de tu reporte. Esta acción no puede ser deshecha.
-                                                <Textarea className="mt-4"/>
-                                            </DialogDescription>
-                                        </DialogHeader>
-                                        <DialogFooter>
-                                            <DialogClose asChild>
-                                                <Button variant="outline" className="cursor-pointer">Cancelar</Button>
-                                            </DialogClose>
-                                            <DialogClose asChild>
-                                                <Button className="cursor-pointer bg-red-500 text-white">Reportar</Button>
-                                            </DialogClose>
-                                        </DialogFooter>
-                                    </DialogContent>
-                                </Dialog>
-
-                                <Dialog>
-                                    <DialogTrigger asChild>
-                                        <DropdownMenuItem className="cursor-pointer" onSelect={e => e.preventDefault()}>
-                                            <UserLock /> <span className="w-full">Bloquear</span>
-                                        </DropdownMenuItem>
-                                    </DialogTrigger>
-                                    <DialogContent>
-                                        <DialogHeader>
-                                            <DialogTitle>¿Estás seguro que quieres bloquear a este usuario?</DialogTitle>
-                                            <DialogDescription>
-                                                Después del bloqueo reajustaremos su Feed para que no te muestre publicaciones del usuario.
-                                            </DialogDescription>
-                                        </DialogHeader>
-                                        <DialogFooter>
-                                            <DialogClose asChild>
-                                                <Button variant="outline" className="cursor-pointer">Cancelar</Button>
-                                            </DialogClose>
-                                            <DialogClose asChild>
-                                                <Button className="cursor-pointer">Bloquear</Button>
-                                            </DialogClose>
-                                        </DialogFooter>
-                                    </DialogContent>
-                                </Dialog>
-
-                                <DropdownMenuItem className="cursor-pointer" onSelect={e => e.preventDefault()} onClick={() =>
-                                    toast("Se ha guardado la publicación", {
-                                        description: new Date().toLocaleString(),
-                                        action: {
-                                            label: "Deshacer",
-                                            onClick: () => console.log("Deshizo")
-                                        }
-                                    })
-                                }>
-                                    <Bookmark /> <span className="w-full">Guardar</span>
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
+                            <DropMenuPost/>
                         </div>
                         </div>
                         <div className="flex flex-col w-full">
