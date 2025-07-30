a957932e097e98463508c0324c7dc190e6ccb60b
RamCodeZ3 <arammusset7@gmail.com>
2025-07-30 14:41:35 -0400

    cambio en home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
 
       <div className="post__container">
         <div className="post__items">
-          <div className="posts w-2xl mx-auto flex flex-col items-center gap-10 max-md:w-full">
+          <div className="posts w-2xl mx-auto flex flex-col items-center gap-10 max-lg:w-full">
             {[1, 2, 3, 4].map((i) => {
               return <Post key={i} />;
             })}
