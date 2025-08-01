6dca8a600805007afc719d6869c6a38593c13594
Mathias Rendon <mathiassaid7@gmail.com>
2025-08-01 18:47:19 -0500

    feat: add custom hook to share posts

---
+++ src/hooks/useShare.tsx
@@ -0,0 +1,23 @@
+import { useCallback } from "react";
+
+interface SharedData {
+    title: string,
+    text: string,
+    url: string,
+}
+
+export const useShare = () => {
+    const share = useCallback(async (data: SharedData) => {
+        if (navigator.share) {
+            try {
+                await navigator.share(data)
+            } catch (error) {
+                console.error(error)
+            }
+        } else {
+            console.log('Web Share API no disponible :(')
+        }
+    }, [])
+
+    return share
+}
