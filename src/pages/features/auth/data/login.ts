b262e8484d2cc6a3865320feb816962db1a9a695
Mathias Rendon <mathiassaid7@gmail.com>
2025-08-04 14:03:50 -0500

    feat: add login functionality connecting to backend

--- src/pages/features/auth/data/login.ts
+++ src/pages/features/auth/data/login.ts
@@ -1,6 +1,33 @@
+import type { NavigateFunction } from "react-router-dom"
 import { z } from "zod"
 
 export const loginSchema = z.object({
     email: z.string().email(),
     password: z.string().min(8)
 })
+
+export const userLogIn = async (email: string, password: string, navigate: NavigateFunction) => {
+    try {
+        const response = await fetch("http://127.0.0.1:8000/api/auth/login/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+        const data = await response.json()
+        if (response.ok) {
+            console.log(data)
+            localStorage.setItem('token', data.token)
+            navigate('/home')
+        } else {
+            console.error("Error en el servidor: ", data)
+            console.log(response.status)
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
