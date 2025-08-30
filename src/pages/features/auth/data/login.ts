7027777d189d7ad329dc062fbd3b5a64bf694400
Mathias Rendon <mathiassaid7@gmail.com>
2025-08-30 17:50:30 -0500

    feat: remove login function and call it from useAuth hook

--- src/pages/features/auth/data/login.ts
+++ src/pages/features/auth/data/login.ts
@@ -1,34 +1,6 @@
-import type { NavigateFunction } from "react-router-dom"
 import { z } from "zod"
 
 export const loginSchema = z.object({
     email: z.string().email(),
     password: z.string().min(8)
 })
-
-export const userLogIn = async (email: string, password: string, navigate: NavigateFunction, showToastMessage: any) => {
-    try {
-        const response = await fetch("http://127.0.0.1:8000/api/auth/login/", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password
-            })
-        })
-        const data = await response.json()
-        if (response.ok) {
-            console.log(data)
-            localStorage.setItem('token', data.token)
-            navigate('/home')
-        } else {
-            showToastMessage("Ha ocurrido un error", "error")
-            console.log(response.status)
-        }
-    } catch (error) {
-        showToastMessage("Ha ocurrido un error", "error")
-        console.error(error)
-    }
-}
