4ae2a3ac2fab64c28c334d58864de7377e871511
Mathias Rendon <mathiassaid7@gmail.com>
2025-08-30 17:49:55 -0500

    feat: create ProtectedRoutes and assign it in routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -55,7 +55,11 @@ import Following from '@/pages/features/social/pages/Following'
 import Saved from '@/pages/features/profile/pages/Saved'
 import Explore from '@/pages/features/social/pages/Explore'
 import Comments from '@/pages/features/comments/pages/Comments'
+<<<<<<< HEAD
 >>>>>>> 6fadaf4 (feat: add params to routes like profile to get data from user)
+=======
+import ProtectedRoutes from './utils/ProtectedRoutes'
+>>>>>>> 82299c4 (feat: create ProtectedRoutes and assign it in routes.tsx)
 
 >>>>>>> be6c850 (Creacion de la pagina Following y estilo base de los contenedores de los usuarios)
 const routes = [
@@ -66,7 +70,11 @@ const routes = [
     },
     {
         path: '/home',
-        element: <SidenavLayout />,
+        element: (
+        <ProtectedRoutes>
+            <SidenavLayout />
+        </ProtectedRoutes>
+        ),
         errorElement: <Error />,
         children: [    
             {
@@ -81,16 +89,32 @@ const routes = [
                 ]
 =======
                 path: 'messages/:id',
+<<<<<<< HEAD
                 element: <Messages />
 >>>>>>> e955c26 (refactor: change messages route)
+=======
+                element: (
+                    <ProtectedRoutes>
+                        <Messages />
+                    </ProtectedRoutes>
+                )
+>>>>>>> 82299c4 (feat: create ProtectedRoutes and assign it in routes.tsx)
             },
             {
                 path: 'profile/:id',
-                element: <Profile />,
+                element: (
+                    <ProtectedRoutes>
+                        <Profile />
+                    </ProtectedRoutes>
+                ),
             },
             {
                 path: 'notifications',
-                element: <Notifications />
+                element: (
+                    <ProtectedRoutes>
+                        <Notifications />
+                    </ProtectedRoutes>
+                )
             },
             {
 <<<<<<< HEAD
@@ -107,29 +131,57 @@ const routes = [
 >>>>>>> be6c850 (Creacion de la pagina Following y estilo base de los contenedores de los usuarios)
 =======
                 path: 'following/:id',
+<<<<<<< HEAD
                 element: <Following/>
 <<<<<<< HEAD
 >>>>>>> 11e50e3 (cambio en la ruta ahora se accede a la pagina de siguiendo en perfil en el apartado de siguiendo y se le añadio un hover a ese mismo texto)
 =======
+=======
+                element: (
+                    <ProtectedRoutes>
+                        <Following/>
+                    </ProtectedRoutes>
+                )
+>>>>>>> 82299c4 (feat: create ProtectedRoutes and assign it in routes.tsx)
             },
             {
                 path: 'new-post',
-                element: <NewPost/>,
+                element: (
+                    <ProtectedRoutes>
+                        <NewPost/>
+                    </ProtectedRoutes>
+                ),
             },
             {
                 path: 'configuration',
+<<<<<<< HEAD
                 element: <Config />
 <<<<<<< HEAD
 >>>>>>> 3809200 (fix: keep staged changes with the new changes in)
 =======
+=======
+                element: (
+                    <ProtectedRoutes>
+                        <Config />
+                    </ProtectedRoutes>
+                )
+>>>>>>> 82299c4 (feat: create ProtectedRoutes and assign it in routes.tsx)
             },
             {
                 path: 'saved',
-                element: <Saved />
+                element: (
+                    <ProtectedRoutes>
+                        <Saved />
+                    </ProtectedRoutes>
+                )
             },
             {
                 path: 'explore',
-                element: <Explore />
+                element: (
+                    <ProtectedRoutes>
+                        <Explore />
+                    </ProtectedRoutes>
+                )
             },
             {
 <<<<<<< HEAD
@@ -144,7 +196,11 @@ const routes = [
 =======
 >>>>>>> 6fadaf4 (feat: add params to routes like profile to get data from user)
                 path: 'comment/:id',
-                element: <Comments/>
+                element: (
+                    <ProtectedRoutes>
+                        <Comments/>
+                    </ProtectedRoutes>
+                )
             },
 >>>>>>> c2b8c38 (Creacion del componente Comments, donde se vera los comentarios de la publicaciones.)
         ]
